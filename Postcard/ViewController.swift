1f5237629ee17e89025218abfd5fb320f8bbb624
Mireia <mireia@mireiasangalo.com>
2014-10-31 16:43:23 +0000

    New namelabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -31,11 +32,19 @@ class ViewController: UIViewController {
         
         //Adding a comment here to test commits.
         messageLabel.hidden = false
+        nameLabel.hidden = false
+        
         messageLabel.text = enterMessageTextField.text
+        nameLabel.text = enterNameTextField.text
+        
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.textColor = UIColor.blueColor()
         
         enterMessageTextField.text = "" //Treu el texte escrit a Enter Message
         enterMessageTextField.resignFirstResponder() //Per treure el teclat de la pantalla quan fs click "Send"
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
